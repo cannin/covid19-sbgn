2e1ae04e11ed618f1dcb126a0216a079b248fde6
cannin <cannin@gmail.com>
2020-04-07 16:36:53 -0400

    Try to download cd2sbgnml

--- java/cd2sbgnml.sh
+++ java/cd2sbgnml.sh
@@ -1,5 +1,10 @@
+# Download cd2sbgnml
+wget https://github.com/sbgn/cd2sbgnml/releases/download/0.4.5/cd2sbgnml-0.4.5-app.jar
+
+# Move CellDesigner files 
 find covid_models/Curation -name "*.xml" -exec cp {} static \;
 
-java -cp java/cd2sbgnml-0.4.4-app.jar fr.curie.cd2sbgnml.Cd2SbgnmlScript -i static/COVID19_PAMP_signaling.xml -o static/COVID19_PAMP_signaling.sbgn
-java -cp java/cd2sbgnml-0.4.4-app.jar fr.curie.cd2sbgnml.Cd2SbgnmlScript -i static/Viral_entry_V01_3.xml -o static/Viral_entry_V01_3.sbgn
-java -cp java/cd2sbgnml-0.4.4-app.jar fr.curie.cd2sbgnml.Cd2SbgnmlScript -i static/Orf10_Cul2_pathway.xml -o static/Orf10_Cul2_pathway.sbgn
+# Convert files
+java -cp cd2sbgnml-0.4.5-app.jar fr.curie.cd2sbgnml.Cd2SbgnmlScript -i static/COVID19_PAMP_signaling.xml -o static/COVID19_PAMP_signaling.sbgn
+java -cp cd2sbgnml-0.4.5-app.jar fr.curie.cd2sbgnml.Cd2SbgnmlScript -i static/Viral_entry_V01_3.xml -o static/Viral_entry_V01_3.sbgn
+java -cp cd2sbgnml-0.4.5-app.jar fr.curie.cd2sbgnml.Cd2SbgnmlScript -i static/Orf10_Cul2_pathway.xml -o static/Orf10_Cul2_pathway.sbgn
