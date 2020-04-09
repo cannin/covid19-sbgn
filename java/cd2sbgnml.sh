094acd4c4a845d8d36af68be880c582c040202d3
cannin <cannin@gmail.com>
2020-04-09 18:51:01 -0400

    Add git clone command

--- java/cd2sbgnml.sh
+++ java/cd2sbgnml.sh
@@ -1,9 +1,15 @@
 # Download cd2sbgnml
 wget https://github.com/sbgn/cd2sbgnml/releases/download/0.4.5/cd2sbgnml-0.4.5-app.jar
 
+# Download Model 
+git clone https://git-r3lab.uni.lu/covid/models.git covid_models
+
 # Move CellDesigner files 
 find covid_models/Curation -name "*.xml" -exec cp {} static \;
 ls -1 static
+
+# Debug 
+ls -lR .
 
 # Convert files
 java -cp cd2sbgnml-0.4.5-app.jar fr.curie.cd2sbgnml.Cd2SbgnmlScript -i static/COVID19_PAMP_signaling.xml -o static/COVID19_PAMP_signaling.sbgn
