024cf53dbb49531ab26047790d2f9439449e7e1a
cannin <cannin@gmail.com>
2020-04-09 20:08:29 -0400

    Automatically list new files Move setup to Travis

--- java/cd2sbgnml.sh
+++ java/cd2sbgnml.sh
@@ -1,8 +1,8 @@
 # Download cd2sbgnml
-wget https://github.com/sbgn/cd2sbgnml/releases/download/0.4.5/cd2sbgnml-0.4.5-app.jar
+#wget https://github.com/sbgn/cd2sbgnml/releases/download/0.4.5/cd2sbgnml-0.4.5-app.jar
 
 # Download Model 
-git clone https://git-r3lab.uni.lu/covid/models.git covid_models
+#git clone https://git-r3lab.uni.lu/covid/models.git covid_models
 
 # Move CellDesigner files 
 find covid_models/Curation -name "*.xml" -exec cp {} static \;
@@ -14,14 +14,14 @@ ls -lR .
 # Convert files
 #cd static 
 
-for file in *.java; do
+for file in *.xml; do
     [ -f "$file" ] || break
 
     tmp = $(basename "$file" | cut -d. -f1)
 
     echo $file $tmp
     
-    java -cp cd2sbgnml-0.4.5-app.jar fr.curie.cd2sbgnml.Cd2SbgnmlScript -i "static/$file" -o "static/$tmp.sbgn"
+    java -cp java/cd2sbgnml-0.4.5-app.jar fr.curie.cd2sbgnml.Cd2SbgnmlScript -i "static/$file" -o "static/$tmp.sbgn"
 done
 
 #java -cp ../cd2sbgnml-0.4.5-app.jar fr.curie.cd2sbgnml.Cd2SbgnmlScript -i COVID19_PAMP_signaling.xml -o COVID19_PAMP_signaling.sbgn
